779a14e4de7fc840812cd271a48b1c8cf75eeab9
jjaejjae12 <81688137+jjaejjae12@users.noreply.github.com>
2022-06-08 22:59:27 +0900

    hiddenApi
    hiddenApi

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,18 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NavBar from "../components/Navbar";
 import Head from "next/head";
 import Seo from "../components/Seo";
-import { useEffect } from "react/cjs/react.production.min";
-
-const API_KEY = "63b2b1c1f8886b5eda4a8edafdc64504";
 
 export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {}, []);
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(`/api/movies`)).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
-      <button onClick={() => setCounter((prev) => prev + 1)}>Click me</button>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
